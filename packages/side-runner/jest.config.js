8f7f8717b0307e7c5bceb361c18445018a194bf0
zhangyongming@hessianhealth.com <zhangyongming@hessianhealth.com>
2023-11-08 15:41:09 +0800

    修改side-runner

--- packages/side-runner/jest.config.js
+++ packages/side-runner/jest.config.js
@@ -4,20 +4,7 @@ const process = require('process');
 
 module.exports = {
     reporters: [
-        'default',
-        ['jest-junit',{
-            outputDirectory: "D:\\ly\\ws_node\\TS_Demo\\output",
-            outputName: "jest_junit_reporters.html"
-        }],
-        ['sample-html-reporter',{
-            outputDirectory: "D:\\ly\\ws_node\\TS_Demo\\output",
-            outputName: "jest_sample_html_reporters.html"
-        }],
-        '<rootDir>/src/reporter/custom-reporter.js',
-        ["jest-html-reporters", {
-            publicPath: "D:\\ly\\ws_node\\TS_Demo\\output",
-            filename: "jest_html_reporters.html"
-        }],
+        'default'
     ],
     rootDir: path.resolve(__dirname),
     testEnvironment: 'node',
